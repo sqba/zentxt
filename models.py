67b3ea949149b7983159e4d12fb9a1d2f640555d
esqban <esqban@4dd0ef2c-067a-8ee0-5975-2cc3b15f7e80>
2012-02-17 18:37:48 +0000

    linked list

--- models.py
+++ models.py
@@ -1,12 +1,15 @@
 from google.appengine.ext import db
 
+class Revision(db.Model):
+    author = db.UserProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
+    content = db.TextProperty()
+#    diff = db.TextProperty()
+#    file = db.ReferenceProperty(File)
+    file = db.ReferenceProperty(collection_name='file')
+    prev = db.SelfReferenceProperty()
+
 class File(db.Model):
     author = db.UserProperty()
     name = db.StringProperty()
-
-class Revision(db.Model):
-    author = db.UserProperty()
-    content = db.TextProperty()
-    date = db.DateTimeProperty(auto_now_add=True)
-    diff = db.TextProperty()
-    file = db.ReferenceProperty(File)
+    head = db.ReferenceProperty(Revision)
--- revision.py
+++ revision.py
@@ -5,31 +5,16 @@ from base import BasePage
 
 import diff_match_patch as dmp_module
 
-
 dmp = dmp_module.diff_match_patch()
 dmp.Diff_Timeout = 0.1  # Don't spend more than 0.1 seconds on a diff.
 
 
-def distance (sx, sy):
-    diffs = dmp.diff_main(sx, sy)
-    dmp.diff_cleanupSemantic(diffs)
-    return dmp.diff_prettyHtml(diffs)
-
-
 class RevisionPage(BasePage):
 
-    def get_revision_by_id(self, id):
-        rev_key = db.Key(id)
-        query = Revision.gql("WHERE __key__ = :1", rev_key)
-        entities = query.fetch(1)
-        if len(entities) == 0:
-            return None
-        return entities[0]
-
-    def get_previous_revision(self, rev):
-        q = Revision.all().filter('date >', rev.date)
-        prev = q.get()
-        return prev
+    def distance(self, sx, sy):
+        diffs = dmp.diff_main(sx, sy)
+        dmp.diff_cleanupSemantic(diffs)
+        return dmp.diff_prettyHtml(diffs)
 
     def get(self):
         if not self.check_user():
@@ -37,11 +22,13 @@ class RevisionPage(BasePage):
 
         rev = self.get_revision_by_id( self.request.get("id") )
         if rev is None:
+            self.response.out.write("rev not found")
             return
 
-        prev = self.get_previous_revision( rev )
+        prev = rev.prev
         if prev is None:
+            self.response.out.write("prev not found")
             return
 
-        diff = distance(rev.content, prev.content).replace("&para;", "")
+        diff = self.distance(prev.content, rev.content).replace("&para;", "")
         self.response.out.write(diff)
