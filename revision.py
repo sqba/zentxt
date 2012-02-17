2f0968e794b25e27dc5cfb53afecac8572ef60be
esqban <esqban@4dd0ef2c-067a-8ee0-5975-2cc3b15f7e80>
2012-02-17 15:39:32 +0000

    cleanup

--- revision.py
+++ revision.py
@@ -11,7 +11,17 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 from models import File, Revision
 
-from diff import distance
+import diff_match_patch as dmp_module
+
+
+dmp = dmp_module.diff_match_patch()
+dmp.Diff_Timeout = 0.1  # Don't spend more than 0.1 seconds on a diff.
+
+
+def distance (sx, sy):
+    diffs = dmp.diff_main(sx, sy)
+    dmp.diff_cleanupSemantic(diffs)
+    return dmp.diff_prettyHtml(diffs)
 
 
 class RevisionPage(webapp.RequestHandler):
@@ -27,23 +37,16 @@ class RevisionPage(webapp.RequestHandler):
         query = Revision.gql("WHERE __key__ = :1", key_object)
         entities = query.fetch(1)
         revision = entities[0]
-        #text = cgi.escape(revision.content)
         text = revision.content
-        
-        #self.response.out.write(text)
-        #return
 
         q = Revision.all().filter('date >', revision.date)
         tmp = q.get()
         if tmp is None:
 			return
-        #prev = cgi.escape(tmp.content)
         prev = tmp.content
             
         diff = distance(text, prev)
-        #diff = diff.replace("\n", "<br>")
         diff = diff.replace("&para;", "")
-        #diff = diff.replace("<br>", "\n")
         self.response.out.write(diff)
 
     def get(self):
