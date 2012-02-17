24e8f7dd74926b577304f54f381336040bceb549
esqban <esqban@4dd0ef2c-067a-8ee0-5975-2cc3b15f7e80>
2012-02-17 16:57:44 +0000

    base

--- trunk/file.py
+++ trunk/file.py
@@ -5,46 +5,52 @@ import urllib
 from google.appengine.ext.webapp import template
 
 from google.appengine.ext import db
-from google.appengine.api import users
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
 
 from models import File, Revision
+from base import BasePage
 
+class FilePage(BasePage):
 
-class FilePage(webapp.RequestHandler):
-
-    def show_revisions(self):
-        file_id = self.request.get("id")
-        user = users.get_current_user()
-
-        file_text = ""
-        revisions2 = []
-
-        key_object = db.Key(file_id)
+    def get_file(self, id):
+        key_object = db.Key(id)
         query = File.gql("WHERE __key__ = :1", key_object)
         entities = query.fetch(1)
         if len(entities) > 0:
-            file = entities[0]
+            return entities[0]
         else:
-            file = File()
-            file.put()
+            return None
 
+    def get_head(self, file):
         query = Revision.gql("WHERE file = :1 ORDER BY date DESC", file)
         revisions = query.fetch(1)
         if len(revisions) > 0:
-            file_text = cgi.escape(revisions[0].content)
-            last_date = revisions[0].date
-
-            query = Revision.gql("WHERE file = :1 and date < :2 ORDER BY date DESC", file, last_date)
-            revisions = query.fetch(100)
+            return revisions[0]
         else:
+            return None
+
+    def get_revisions(self, rev):
+        query = Revision.gql("WHERE file = :1 and date < :2 ORDER BY date DESC", rev.file, rev.date)
+        return query.fetch(100)
+
+    def show_revisions(self):
+        file_id = self.request.get("id")
+
+        file = self.get_file(file_id)
+        if file is None:
+            file = File()
+            file.put()
+
+        head = self.get_head(file)
+        if head is None:
             file_text = "Welcome to ZenTxt!"
             revisions = []
+        else:
+            file_text = cgi.escape(head.content)
+            revisions = self.get_revisions(head)
 
         template_values = {
-            'user' : user,
-            'file_id' : file_id,
+            'user'      : self.get_current_user(),
+            'file_id'   : file_id,
             'revisions' : revisions,
             'file_text' : file_text
         }
@@ -52,35 +58,33 @@ class FilePage(webapp.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'file.html')
         self.response.out.write(template.render(path, template_values))
 
+    def is_text_changed(self, file, text):
+        head = self.get_head( file )
+        if not head is None:
+            return (text == head.content)
+        else:
+            return False
+
+    def save_revision(self):
+        file_id = self.request.get("id")
+        file = self.get_file( file_id )
+        new_text = self.request.get('content')
+
+        if not self.is_text_changed(file, new_text):
+            revision = Revision()
+            revision.author     = self.get_current_user()
+            revision.content    = new_text
+            revision.file       = file
+            revision.put()
+
+        self.redirect('/file?' + urllib.urlencode({'id': file_id}))
+
     def get(self):
-        user = users.get_current_user()
-        if not user:
-            self.redirect(users.create_login_url(self.request.uri))
+        if not self.check_user():
             return
         self.show_revisions()
 
     def post(self):
-        user = users.get_current_user()
-        if not user:
-            self.redirect(users.create_login_url(self.request.uri))
+        if not self.check_user():
             return
-
-        revision = Revision()
-
-        #revision.author = users.get_current_user()
-        revision.content = self.request.get('content')
-        file_id = self.request.get("id")
-        key_object = db.Key(file_id)
-        query = File.gql("WHERE __key__ = :1", key_object)
-        entities = query.fetch(1)
-        revision.file = entities[0]
-
-        # Avoid saving same text
-        query = Revision.gql("WHERE file = :1 ORDER BY date DESC", revision.file)
-        revisions = query.fetch(1)
-        if len(revisions) > 0:
-            last_text = revisions[0].content
-            if revision.content != last_text:
-                revision.put()
-
-        self.redirect('/file?' + urllib.urlencode({'id': file_id}))
+        self.save_revision()
--- trunk/files.py
+++ trunk/files.py
@@ -1,44 +1,34 @@
 import urllib
 
 from google.appengine.ext import db
-from google.appengine.api import users
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
 
 from models import File
+from base import BasePage
 
+class FilesPage(BasePage):
 
-class FilesPage(webapp.RequestHandler):
+    def create_file(self, filename):
+        file = File()
+        file.author = self.get_current_user()
+        file.name = filename
+        key = file.put()
+        self.redirect('/file?' + urllib.urlencode({'id': key}))
+        return key
 
     def show_files(self):
-        user = users.get_current_user()
-        if not user:
-            self.redirect(users.create_login_url(self.request.uri))
-            return
-        files = db.GqlQuery("SELECT * FROM File WHERE author = :1 LIMIT 50", user)
+        files = db.GqlQuery("SELECT * FROM File WHERE author = :1 LIMIT 50", self.get_current_user())
         if files.count() > 0:
             self.redirect('/file?' + urllib.urlencode({'id': files[0].key()}))
         else:
             file_id = self.create_file("New File")
             self.redirect('/file?' + urllib.urlencode({'id': file_id}))
 
-    def create_file(self, filename):
-        file = File()
-        file.author = users.get_current_user()
-        file.name = filename
-        key = file.put()
-        self.redirect('/file?' + urllib.urlencode({'id': key}))
-        return key
-
     def get(self):
-        user = users.get_current_user()
-        if not user:
-            self.redirect(users.create_login_url(self.request.uri))
-        else:
-            self.show_files()
+        if not self.check_user():
+            return
+        self.show_files()
 
     def post(self):
-        user = users.get_current_user()
-        if not user:
-            self.redirect(users.create_login_url(self.request.uri))
+        if not self.check_user():
             return
+        #self.create_file()
