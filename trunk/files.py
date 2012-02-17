5681f7f29aaff3013d93b6753e9e594aa1487811
esqban <esqban@4dd0ef2c-067a-8ee0-5975-2cc3b15f7e80>
2012-02-17 22:42:08 +0000

    home

--- trunk/files.py
+++ trunk/files.py
@@ -10,40 +10,30 @@ import base
 
 from google.appengine.ext.webapp import template
 
-class FilesPage(BasePage):
-
-    def show_files(self):
-        self.response.out.write("show_files")
-        files = db.GqlQuery("SELECT * FROM File WHERE author = :1 LIMIT 50", self.get_current_user())
-        if files.count() > 0:
-            self.redirect('/file?' + urllib.urlencode({'id': files[0].key()}))
-        else:
-            if self.check_user():
-                file_id = self.create_file("New File")
-            else:
-                self.redirect('/home')
-
-    def get(self):
-#        if not self.check_user():
-#            return
-        self.show_files()
-
-#    def post(self):
-#        if not self.check_user():
-#            return
-        #self.create_file()
 
 class HomePage(BasePage):
-    def get(self):
-        if self.check_user(False):
-            self.redirect('/')
-            return
+
+    def logged_user_home(self):
+        files = db.GqlQuery("SELECT * FROM File WHERE author = :1 LIMIT 50", self.get_current_user())
+        if files.count() > 0:
+            file_id = files[0].key()
+        else:
+            file_id = self.create_file("New File")
+        self.redirect('/file?' + urllib.urlencode({'id': files[0].key()}))
+
+    def anon_user_page(self):
         template_values = {
             'user'      : self.get_current_user(),
             'login_url' : users.create_login_url(self.request.uri)
         }
         path = os.path.join(os.path.dirname(__file__), 'home.html')
         self.response.out.write(template.render(path, template_values))
+
+    def get(self):
+        if self.check_user(False):
+            self.logged_user_home()
+        else:
+            self.anon_user_page()
 
 class SuggestionsPage(BasePage):
     def get(self):
