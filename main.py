9aae0ff1c1351f975e645a688214bc1a360785ed
esqban@gmail.com <esqban@gmail.com@4dd0ef2c-067a-8ee0-5975-2cc3b15f7e80>
2012-02-18 11:25:19 +0000

    files

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import wsgiref.handlers
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-from files import HomePage, SuggestionsPage
+from home import HomePage, SuggestionsPage
+from files import FilesPage
 from file import FilePage
 from revision import RevisionPage
 
@@ -12,6 +13,7 @@ application = webapp.WSGIApplication([
     ('/', HomePage),
     ('/file', FilePage),
     ('/save', FilePage),
+    ('/files', FilesPage),
     ('/rev', RevisionPage),
     ('/revert', RevisionPage),
     ('/suggestions', SuggestionsPage)
